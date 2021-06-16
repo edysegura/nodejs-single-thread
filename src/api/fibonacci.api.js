191bd2ad4587852aa7a9e629c5efe25ecb607b09
Edy Segura <ednardo.segura@mundiale.com.br>
2021-06-16 08:24:18 -0300

    Add express

--- src/api/fibonacci.api.js
+++ src/api/fibonacci.api.js
@@ -1,5 +1,17 @@
+import express from 'express'
 import { calculateFibonacci } from './fibonacci.js'
 
-const start = Date.now()
-console.log(calculateFibonacci(0))
-console.log('Hi there! After', Date.now() - start)
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (_, response) => {
+    response.send('Hi there!')
+})
+
+app.listen(port, () => {
+    console.log(`Server up and listening at http://localhost:${port}`)
+})
+
+// const start = Date.now()
+// console.log(calculateFibonacci(0))
+// console.log('Hi there! After', Date.now() - start)
