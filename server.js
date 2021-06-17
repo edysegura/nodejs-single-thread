71f5147578c1987c9a95745132e5659a9b1e4c36
Edy Segura <ednardo.segura@mundiale.com.br>
2021-06-16 21:56:30 -0300

    Add new routes
    - fibonacci
    - hash

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import { calculateFibonacci } from './src/fibonacci.js'
+import { logHashTime } from './src/libuv-threadpool.js'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (_, response) => {
+    response.send('Hi there!')
+})
+
+app.get('/fibonacci/:number', (request, response) => {
+    const number = request.params.number
+    response.send(`The ${number}x of fibonacci interaction is: ${calculateFibonacci(number)}`)
+})
+
+app.get('/hash/:password', (request, response) => {
+    const password = request.params.password
+    response.send(logHashTime(password))
+})
+
+app.listen(port, () => {
+    console.log(`Server up and listening at http://localhost:${port}`)
+})
