a6db76e5661f20613d284b9fe5af91698f6f84e1
Edy Segura <edysegura@gmail.com>
2025-08-20 17:26:22 -0300

    chore: add prettier

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { Worker } from 'worker_threads'
+import { Worker } from 'node:worker_threads'
 
 import { logHashTime } from './src/libuv-threadpool.js'
 
@@ -7,23 +7,23 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (_, response) => {
-    response.send('Hi there!')
+  response.send('Hi there!')
 })
 
 app.get('/fibonacci/:number', (request, response) => {
-    const number = request.params.number
-    const worker = new Worker('./src/worker.js')
-    worker.postMessage(number)
-    worker.once('message', result => {
-        response.send(`The ${number}x of fibonacci interaction is: ${result}`)
-    })
+  const number = request.params.number
+  const worker = new Worker('./src/worker.js')
+  worker.postMessage(number)
+  worker.once('message', (result) => {
+    response.send(`The ${number}x of fibonacci interaction is: ${result}`)
+  })
 })
 
 app.get('/hash/:password', async (request, response) => {
-    const password = request.params.password
-    response.send(await logHashTime(password))
+  const password = request.params.password
+  response.send(await logHashTime(password))
 })
 
 app.listen(port, () => {
-    console.log(`Server up and listening at http://localhost:${port}`)
+  console.log(`Server up and listening at http://localhost:${port}`)
 })
